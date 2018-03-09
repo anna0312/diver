ce787def4ceb5a7fbfd564dd0fdd25ae533f33ae
anna0312 <annabremner@gmail.com>
2018-03-09 10:02:27 -0500

    Completed CURL CRUD

---
+++ scripts/dives/delete-dive.sh
@@ -0,0 +1,6 @@
+curl "http://localhost:4741/dives/$ID" \
+  --include \
+  --request DELETE \
+
+echo
+#  ID=3 sh scripts/dives/delete-dive.sh
---
+++ scripts/dives/get-dive.sh
@@ -0,0 +1,6 @@
+curl "http://localhost:4741/dives/$ID" \
+  --include \
+  --request GET \
+
+echo
+# sh scripts/dives/get-dives.sh
